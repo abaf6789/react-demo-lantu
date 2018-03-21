56f89e89fd2989f3e5ba18e1123d93201a3860f7
lukas <1148490049@qq.com>
2018-03-21 12:02:28 +0800

    增加样式处理

--- src/component/Sidebar/item.jsx
+++ src/component/Sidebar/item.jsx
@@ -4,16 +4,16 @@ import { Menu, Icon } from 'semantic-ui-react'
 export default class Item extends Component {
     render() {
         const IconStyle = { display: "inline-block", fontSize: "12px" }
-        const itemStyle = { textAlign: "start", fontWeight: "100", color: "#aaa", borderLeft: "6px solid #00a5ff", borderRadius: "4px" }
+        const itemStyle = { textAlign: "start", fontWeight: "100", color: "#aaa" }
+        const selectStyle = { textAlign: "start", fontWeight: "100", color: "#aaa", borderLeft: "6px solid #00a5ff", borderRadius: "4px" }
         const index = this.props.index
+        const selectedIndex = this.props.selectedIndex
         const item = this.props.item
         return (
-            <div>
-                <Menu.Item style={itemStyle} name={item.name} key={index} onClick={this.props.clickFn2.bind(this, index)}>
-                    <Icon name="home" style={IconStyle} />
-                    {item.name}
-                </Menu.Item>
-            </div>
+            <Menu.Item style={(selectedIndex === index) ? selectStyle : itemStyle} name={item.name} key={index} onClick={this.props.clickFn2.bind(this, index)}>
+                <Icon name="home" style={IconStyle} />
+                {item.name}
+            </Menu.Item>
         )
     }
 }
--- src/component/Sidebar/siderbar.jsx
+++ src/component/Sidebar/siderbar.jsx
@@ -3,15 +3,19 @@ import { Sidebar, Menu } from 'semantic-ui-react'
 import Item from "./item"
 
 export default class Siderbar extends Component {
+    state = {
+        selectedIndex: ""
+    }
     clickFn2 = (index) => {
         this.props.clickFn(index)
+        this.setState({ selectedIndex: index })
     }
     render() {
         const SidebarStyle = { width: "200px", background: "#373737", textOverflow: "ellipsis", wordBreak: "break-all", whiteSpace: "nowrap", overflow: "hidden", fontSize: "13px" }
         return (
             <Sidebar as={Menu} animation='scale down' width='thin' visible={this.props.visible} icon='labeled' vertical inverted style={SidebarStyle}>
                 {this.props.data.map((item, index) => {
-                    return <Item index={index} item={item} key={index} clickFn2={this.clickFn2} visible={this.props.visible} animation='scale down' />
+                    return <Item selectedIndex={this.state.selectedIndex} index={index} item={item} key={index} clickFn2={this.clickFn2} visible={this.props.visible} animation='scale down' />
                 })}
             </Sidebar>
         )
